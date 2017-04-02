e9d0dac31ecb7599242d7018c2cf1fa86c9f848f
rbruflodt <rachel-bruflodt@uiowa.edu>
2017-04-01 19:53:56 -0500

    initial commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,23 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+/**
+ * Created by Rachel on 4/1/2017.
+ */
+public class LoginTest {
+
+    @Test
+    public void loginTests(){
+        System.setProperty("phantomjs.binary.path","phantomjs.exe");
+        DesiredCapabilities capabilities = DesiredCapabilities.phantomjs();
+        PhantomJSDriver driver = new PhantomJSDriver(capabilities);
+        driver.get("http://iowaair.us-east-1.elasticbeanstalk.com/");
+        driver.manage().window().setSize( new Dimension( 1124, 850 ) );
+        Assert.assertEquals(driver.getCurrentUrl(),"http://iowaair.us-east-1.elasticbeanstalk.com/");
+        Assert.assertEquals(driver.getTitle(),"Iowa Air");
+    }
+}
